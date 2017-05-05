6aeda820325e071e624dbbabe9db038ab0bf1aae
Bo Molocznik <bo@axismedtech.com>
2017-05-04 17:38:04 -0700

    bump default for now to build things.

--- action/src/main/java/move/action/ActionConfig.java
+++ action/src/main/java/move/action/ActionConfig.java
@@ -29,7 +29,7 @@ public @interface ActionConfig {
     /**
      * @return
      */
-    int maxExecutionMillis() default 20_000;
+    int maxExecutionMillis() default 120_000;
 
     /**
      * @return
